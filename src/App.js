ff959393822f84d2d9ff7100ce356346322adf85
Marijn Baar <marijnbaar@gmail.com>
2020-10-23 15:54:18 +0200

    added font for header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <div className="container">
         <div class="no-bears-logo">
         </div>
-
         <div class="jager-cor">
           <img class="cover" src="https://picsum.photos/id/1021/705/460" alt="mountains" />
         </div>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Header() {
-    return (
-      <div className="header-wall-of-fame">
-        <h1>Wall of fame.</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="header-wall-of-fame">
+      <hr className="hr-line"/>
+      <h1 className="header-page">Wall of fame.</h1>
+    </div>
+  );
+}
 
 export default Header;
