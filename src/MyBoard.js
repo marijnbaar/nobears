fcbe3db1059bb00b170d2257fe4011ec80b81bb5
Marijn Baar <marijnbaar@gmail.com>
2020-10-24 23:36:03 +0200

    added images for board component

--- src/MyBoard.js
+++ src/MyBoard.js
@@ -4,10 +4,69 @@ import Card from './Card.js'
 
 const imageArray = [
   {
-    src: "https://picsum.photos/id/1021/705/460",
-    alt: "mountains"
+    src: "https://i.ibb.co/SX76sdN/Group-43.png", 
+    alt: "black-bear"
+  },
+  {
+    src: "https://i.ibb.co/6v3kfTS/Mask-Group-7.png", 
+    alt: "cor-met-geweer"
+  },
+  {
+    src: "https://i.ibb.co/R7s4ybk/Mask-Group-9.png", 
+    alt: "bijl"
+  },
+  {
+    src: "https://i.ibb.co/VMwSYG3/Group-49.png", 
+    alt: "vacht"
+  },
+  {
+    src: "https://i.ibb.co/5Kwtdfn/Mask-Group-3.png", 
+    alt: "vacht"
+  },
+  {
+    src: "https://i.ibb.co/DMLJ3wp/Mask-Group-11.png", 
+    alt: "handschoenen"
+  },
+  {
+    src: "https://i.ibb.co/ncSb3fx/Mask-Group-6.png", 
+    alt: "lamp"
+  },
+  {
+    src: "https://i.ibb.co/Tt9W4T1/Group-48.png", 
+    alt: "illustratie-geweer"
+  },
+  {
+    src: "https://i.ibb.co/BnC6f3s/Mask-Group-8.png", 
+    alt: "cor-met-hond"
+  },
+  {
+    src: "https://i.ibb.co/HVcTFxL/Mask-Group-4.png", 
+    alt: "geweer"
+  },
+  {
+    src: "https://i.ibb.co/JkYRXLh/Group-44.png", 
+    alt: "alles-vangen-we-af"
+  },
+  {
+    src: "https://i.ibb.co/r2Zgt46/Group-45.png", 
+    alt: "verstopte-beer"
+  },
+  {
+    src: "https://i.ibb.co/wS9fcx8/Group-47.png", 
+    alt: "jacht"
+  },
+  {
+    src: "https://i.ibb.co/DLz21YQ/Mask-Group-10.png", 
+    alt: "hond"
+  },
+  {
+    src: "https://i.ibb.co/Z19bmLq/Mask-Group-5.png", 
+    alt: "muis"
+  },
+  {
+    src: "https://i.ibb.co/JQfGyrC/Group-46.png", 
+    alt: "groen-nobears"
   }
-
 ]
 
   function MyBoard() {
