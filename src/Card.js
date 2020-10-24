f3c39ee919736c9d17f2dd8c47ce1bc62768571d
Marijn Baar <marijnbaar@gmail.com>
2020-10-24 21:06:48 +0200

    made the card

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Card.css'
 import Image from "react-bootstrap/Image"
 import dots from "../src/assets/dots.svg"
 import heart from "../src/assets/heart.svg"
 import download from "../src/assets/download.svg"
 
-function Card({ title }) {
+function Card() {
     return (
-        <div className="card-container">
-            <p className="title">{title}</p>
-            <div className="welcome-cor">
-                <div className="card-header">
-                    {/* <div className="images-header"> */}
+            <div className="card-container">
+                <div className="welcome-cor">
+                    <div className="card-header">
                         <Image
                             src={dots}
                             width="41"
@@ -34,27 +31,25 @@ function Card({ title }) {
                             className="download"
                             alt="download"
                         />
-                    {/* </div> */}
-                </div>
-                <img className="cover" src="https://picsum.photos/id/1021/934/526" alt="mountains" />
-                <div className="card-text">
-                    <h2 className="welcome">Aangenaam,</h2>
-                    <h1 className="name">Cor Hunter</h1>
-                    <p className="lorem-ipsum">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus augue lacus, tristique sit amet tellus in, varius porttitor est.
-                        Aliquam erat volutpat. Vivamus ac aliquet libero. Fusce dapibus, enim vitae congue pulvinar, augue eros imperdiet justo, et facilisis diam lectus non velit.
-                        Donec facilisis viverra fringilla. In est orci, ullamcorper eget tincidunt nec, ornare quis elit.
-                        Morbi arcu eros, ullamcorper eget odio sed, euismod sollicitudin eros. Proin vitae suscipit purus, nec blandit tellus. Vivamus porta sapien magna,
-                        ac molestie ex aliquet nec. Nulla facilisi. Donec vitae suscipit arcu. Donec convallis tincidunt pellentesque. Phasellus tincidunt nibh a nulla vulputate cursus.
+                        <button className="save">Bewaren</button>
+                    </div>
+                    <img className="cover" src="https://picsum.photos/id/1021/934/526" alt="mountains" />
+                    <div className="content-container">
+                        <div className="card-text">
+                            <h2 className="welcome">Aangenaam,</h2>
+                            <h1 className="name">Cor Hunter</h1>
+                            <p className="lorem-ipsum">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus augue lacus, tristique sit amet tellus in, varius porttitor est.
+                                Aliquam erat volutpat. Vivamus ac aliquet libero. Fusce dapibus, enim vitae congue pulvinar, augue eros imperdiet justo, et facilisis diam lectus non velit.
+                                Donec facilisis viverra fringilla. In est orci, ullamcorper eget tincidunt nec, ornare quis elit.
+                                Morbi arcu eros, ullamcorper eget odio sed, euismod sollicitudin eros. Proin vitae suscipit purus, nec blandit tellus. Vivamus porta sapien magna,
+                                ac molestie ex aliquet nec. Nulla facilisi. Donec vitae suscipit arcu. Donec convallis tincidunt pellentesque. Phasellus tincidunt nibh a nulla vulputate cursus.
                 </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
-
-Card.propTypes = {
-    title: PropTypes.string.isRequired,
-};
 
 export default Card;
