59ba2318e47f924d2e765346a46926ed0e19c37a
Marijn Baar <marijnbaar@gmail.com>
2020-10-25 00:03:12 +0200

    changed css cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import MyBoard from './MyBoard.js'
-import Card from './Card.js'
 import Header from './Header';
 import Navbarhome from './Navbar';
 
