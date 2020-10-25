95b2113bfb39400b798539643e02a70e7429a855
Marijn Baar <marijnbaar@gmail.com>
2020-10-25 16:45:47 +0100

    added mediaqueries home tiles

--- src/MyBoard.js
+++ src/MyBoard.js
@@ -5,72 +5,72 @@ import './MyBoard.css'
 
 const imageArray = [
   {
-    src: "https://i.ibb.co/SX76sdN/Group-43.png", 
+    src: "https://i.ibb.co/SX76sdN/Group-43.png",
     alt: "black-bear"
   },
   {
-    src: "https://i.ibb.co/6v3kfTS/Mask-Group-7.png", 
-    alt: "cor-met-geweer"
-  },
-  {
-    src: "https://i.ibb.co/R7s4ybk/Mask-Group-9.png", 
-    alt: "bijl"
-  },
-  {
-    src: "https://i.ibb.co/VMwSYG3/Group-49.png", 
-    alt: "vacht"
-  },
-  {
-    src: "https://i.ibb.co/5Kwtdfn/Mask-Group-3.png", 
-    alt: "vacht"
-  },
-  {
-    src: "https://i.ibb.co/DMLJ3wp/Mask-Group-11.png", 
-    alt: "handschoenen"
-  },
-  {
-    src: "https://i.ibb.co/ncSb3fx/Mask-Group-6.png", 
-    alt: "lamp"
-  },
-  {
-    src: "https://i.ibb.co/Tt9W4T1/Group-48.png", 
-    alt: "illustratie-geweer"
-  },
-  {
-    src: "https://i.ibb.co/BnC6f3s/Mask-Group-8.png", 
-    alt: "cor-met-hond"
-  },
-  {
-    src: "https://i.ibb.co/HVcTFxL/Mask-Group-4.png", 
-    alt: "geweer"
-  },
-  {
-    src: "https://i.ibb.co/JkYRXLh/Group-44.png", 
+    src: "https://i.ibb.co/JkYRXLh/Group-44.png",
     alt: "alles-vangen-we-af"
   },
   {
-    src: "https://i.ibb.co/r2Zgt46/Group-45.png", 
+    src: "https://i.ibb.co/HVcTFxL/Mask-Group-4.png",
+    alt: "geweer"
+  },
+  {
+    src: "https://i.ibb.co/6v3kfTS/Mask-Group-7.png",
+    alt: "cor-met-geweer"
+  },
+  {
+    src: "https://i.ibb.co/DMLJ3wp/Mask-Group-11.png",
+    alt: "handschoenen"
+  },
+  {
+    src: "https://i.ibb.co/r2Zgt46/Group-45.png",
     alt: "verstopte-beer"
   },
   {
-    src: "https://i.ibb.co/wS9fcx8/Group-47.png", 
+    src: "https://i.ibb.co/JQfGyrC/Group-46.png",
+    alt: "groen-nobears"
+  },
+  {
+    src: "https://i.ibb.co/R7s4ybk/Mask-Group-9.png",
+    alt: "bijl"
+  },
+  {
+    src: "https://i.ibb.co/ncSb3fx/Mask-Group-6.png",
+    alt: "lamp"
+  },
+  {
+    src: "https://i.ibb.co/wS9fcx8/Group-47.png",
     alt: "jacht"
   },
   {
-    src: "https://i.ibb.co/DLz21YQ/Mask-Group-10.png", 
+    src: "https://i.ibb.co/VMwSYG3/Group-49.png",
+    alt: "hoedje"
+  },
+  {
+    src: "https://i.ibb.co/Tt9W4T1/Group-48.png",
+    alt: "illustratie-geweer"
+  },
+  {
+    src: "https://i.ibb.co/DLz21YQ/Mask-Group-10.png",
     alt: "hond"
   },
   {
-    src: "https://i.ibb.co/Z19bmLq/Mask-Group-5.png", 
-    alt: "muis"
+    src: "https://i.ibb.co/5Kwtdfn/Mask-Group-3.png",
+    alt: "vacht"
   },
   {
-    src: "https://i.ibb.co/JQfGyrC/Group-46.png", 
-    alt: "groen-nobears"
-  }
+    src: "https://i.ibb.co/BnC6f3s/Mask-Group-8.png",
+    alt: "cor-met-hond"
+  },
+  {
+    src: "https://i.ibb.co/Z19bmLq/Mask-Group-5.png",
+    alt: "muis"
+  },
 ]
 
-  function MyBoard() {
+function MyBoard() {
   const [card, setCard] = React.useState(null);
   const cardClick = (event) => {
     setCard(event.target.src)
@@ -84,10 +84,13 @@ const imageArray = [
       {card ? <Card src={card} closeCard={closeCard} /> : null}
       <div className="container">
         {imageArray.map(image => (
-        <div className="thumbnail">
+          <div className="thumbnail">
             <img onClick={cardClick} src={image.src} alt={image.alt}></img>
-            </div>
-            ))}
+          </div>
+        ))}
+        <span class="thumbnail break"></span>
+        <span class="thumbnail break"></span>
+        <span class="thumbnail break"></span>
       </div>
     </div>
   );
