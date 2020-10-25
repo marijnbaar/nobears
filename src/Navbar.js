2a161abb832094671a3ddf196dfb05b469ff7d87
Marijn Baar <marijnbaar@gmail.com>
2020-10-25 21:17:35 +0100

    edited size card container for all iPhone sizes in Card component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from "react-bootstrap/Image"
 import logo from "../src/assets/logo.svg"
+import searchbar from "../src/assets/searchbar.svg"
+import './Navbar.css'
 
 function Navbarhome() {
     return (
@@ -12,7 +14,10 @@ function Navbarhome() {
                 className="logo"
                 alt="NOBEARS logo"
             />
-            <input className="search-bar" type="text" placeholder="Zoeken" />
+            <div class="search-wrapper">
+            <input className="search-input" type="text" placeholder="Zoeken" />
+                    <img class="search-pic" src={searchbar}/>
+                </div>
         </div>
     );
 }
