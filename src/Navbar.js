6396308b5b8e579de694cac25c509163463689e3
Marijn Baar <marijnbaar@gmail.com>
2020-10-26 14:57:27 +0100

    edited nav bar and added media queries

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from "react-bootstrap/Image"
 import logo from "../src/assets/logo.svg"
+import message from "../src/assets/message.svg"
+import notification from "../src/assets/notification.svg"
+import profile from "../src/assets/profile.png"
 import searchbar from "../src/assets/searchbar.svg"
 import './Navbar.css'
 
@@ -15,9 +18,30 @@ function Navbarhome() {
                 alt="NOBEARS logo"
             />
             <div class="search-wrapper">
-            <input className="search-input" type="text" placeholder="Zoeken" />
-                    <img class="search-pic" src={searchbar}/>
-                </div>
+                <input className="search-input" type="text" placeholder="Zoeken" />
+                <img class="search-pic" src={searchbar} />
+            </div>
+            <Image
+                src={message}
+                width="32"
+                height="29"
+                className="message"
+                alt="message"
+            />
+            <Image
+                src={notification}
+                width="23"
+                height="29"
+                className="notification"
+                alt="notification"
+            />
+            <Image
+                src={profile}
+                width="50"
+                height="39"
+                className="profile"
+                alt="profile"
+            />
         </div>
     );
 }
